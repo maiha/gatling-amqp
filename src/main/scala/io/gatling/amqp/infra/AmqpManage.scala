0b8391425ed8afc447b43df78e137ae79f9f4ee3
Ľubomír Varga <lubomir.varga@nike.sk>
2016-05-12 15:48:19 +0200

    Added better info message for case, when there is already exchange present (with different attributes).

--- src/main/scala/io/gatling/amqp/infra/AmqpManage.scala
+++ src/main/scala/io/gatling/amqp/infra/AmqpManage.scala
@@ -1,17 +1,34 @@
 package io.gatling.amqp.infra
 
-import akka.actor.{ActorRef, Props}
+import java.io.IOException
+
+import akka.actor.Props
 import io.gatling.amqp.config._
 import io.gatling.amqp.data._
 import io.gatling.core.result.writer.StatsEngine
 
 import scala.collection.JavaConversions._
+import scala.util.{Failure, Try}
 
 class AmqpManage(statsEngine: StatsEngine)(implicit amqp: AmqpProtocol) extends AmqpActor {
   override def receive = {
     case msg@ DeclareExchange(AmqpExchange(name, tpe, durable, autoDelete, arguments)) =>
       log.info(s"Initializing AMQP exchange $name")
-      interact(msg) { _.exchangeDeclare(name, tpe, durable, autoDelete, arguments) }
+      interact(msg) { ch =>
+        Try(ch.exchangeDeclare(name, tpe, durable, autoDelete, arguments)) match {
+          case Failure(ex) =>
+            ex match {
+              case ex: IOException if Try(ex.getCause.getMessage).map(_.contains("reply-text=PRECONDITION_FAILED")).getOrElse(false) =>
+                //channel error; protocol method: #method<channel.close>(reply-code=406, reply-text=PRECONDITION_FAILED - inequivalent arg 'auto_delete' for exchange 'luvar@local' in vhost '/': received 'true' but current is 'false', class-id=40, method-id=10)
+                log.error("Declaring exchange, which is already there, but with different parameters! You can delete " +
+                  "exchange and run simulation again, or change parameters of exchange in simulation to match existing " +
+                  "exchange.", ex.getCause)
+              case ex =>
+                log.error("Unexpected failure!", ex)
+            }
+          case _ =>
+        }
+      }
 
     case msg@ DeclareQueue(AmqpQueue(name, durable, exclusive, autoDelete, arguments)) =>
       log.info(s"Initializing AMQP queue $name")
