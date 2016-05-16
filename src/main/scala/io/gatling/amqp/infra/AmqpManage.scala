5794721d59d9cea93105f6641a5f6181bd44998d
Ľubomír Varga <lubomir.varga@nike.sk>
2016-05-16 08:11:59 +0200

    Extended handling of precondition failed error also for queue declaration and binding declaration (I am not sure, if given error can happen also in binding).

--- src/main/scala/io/gatling/amqp/infra/AmqpManage.scala
+++ src/main/scala/io/gatling/amqp/infra/AmqpManage.scala
@@ -3,43 +3,53 @@ package io.gatling.amqp.infra
 import java.io.IOException
 
 import akka.actor.Props
+import com.typesafe.scalalogging.Logger
 import io.gatling.amqp.config._
 import io.gatling.amqp.data._
+import io.gatling.amqp.infra.AmqpManage.handleExceptions
 import io.gatling.core.result.writer.StatsEngine
 
 import scala.collection.JavaConversions._
 import scala.util.{Failure, Try}
 
 class AmqpManage(statsEngine: StatsEngine)(implicit amqp: AmqpProtocol) extends AmqpActor {
+
   override def receive = {
     case msg@ DeclareExchange(AmqpExchange(name, tpe, durable, autoDelete, arguments)) =>
       log.info(s"Initializing AMQP exchange $name")
       interact(msg) { ch =>
-        Try(ch.exchangeDeclare(name, tpe, durable, autoDelete, arguments)) match {
-          case Failure(ex) =>
-            ex match {
-              case ex: IOException if Try(ex.getCause.getMessage).map(_.contains("reply-text=PRECONDITION_FAILED")).getOrElse(false) =>
-                //channel error; protocol method: #method<channel.close>(reply-code=406, reply-text=PRECONDITION_FAILED - inequivalent arg 'auto_delete' for exchange 'luvar@local' in vhost '/': received 'true' but current is 'false', class-id=40, method-id=10)
-                log.error("Declaring exchange, which is already there, but with different parameters! You can delete " +
-                  "exchange and run simulation again, or change parameters of exchange in simulation to match existing " +
-                  "exchange.", ex.getCause)
-              case ex =>
-                log.error("Unexpected failure!", ex)
-            }
-          case _ =>
+        handleExceptions(log, "exchange initialization", Try(ch.exchangeDeclare(name, tpe, durable, autoDelete, arguments)))
         }
-      }
 
     case msg@ DeclareQueue(AmqpQueue(name, durable, exclusive, autoDelete, arguments)) =>
       log.info(s"Initializing AMQP queue $name")
-      interact(msg) { _.queueDeclare(name, durable, exclusive, autoDelete, arguments) }
+      interact(msg) { ch =>
+        handleExceptions(log, "queue initialization", Try(ch.queueDeclare(name, durable, exclusive, autoDelete, arguments)))
+      }
 
     case msg@ BindQueue(exchange, queue, routingKey, arguments) =>
       log.info(s"Initializing AMQP binding $exchange to $queue")
-      interact(msg) { _.queueBind(queue.name, exchange.name, routingKey, arguments) }
+      interact(msg) { ch =>
+        handleExceptions(log, "binding initialization", Try(ch.queueBind(queue.name, exchange.name, routingKey, arguments)))
+      }
   }
 }
 
 object AmqpManage {
-  def props(statsEngine : StatsEngine, amqp: AmqpProtocol) = Props(classOf[AmqpManage], statsEngine, amqp)
+  def props(statsEngine: StatsEngine, amqp: AmqpProtocol) = Props(classOf[AmqpManage], statsEngine, amqp)
+
+  def handleExceptions(log: Logger, operation: String, triedOk: Try[AnyRef]): Unit = {
+    triedOk match {
+      case Failure(ex) =>
+        ex match {
+          case ex: IOException if Try(ex.getCause.getMessage).map(_.contains("reply-text=PRECONDITION_FAILED")).getOrElse(false) =>
+            //channel error; protocol method: #method<channel.close>(reply-code=406, reply-text=PRECONDITION_FAILED - inequivalent arg 'auto_delete' for exchange 'luvar@local' in vhost '/': received 'true' but current is 'false', class-id=40, method-id=10)
+            log.error("Executing " + operation + " failed. Operating on item which is already there, but with different parameters! You can delete " +
+              "given item and run simulation again, or change parameters of item in simulation to match existing one.", ex.getCause)
+          case ex =>
+            log.error("Unexpected failure!", ex)
+        }
+      case _ =>
+    }
+  }
 }
