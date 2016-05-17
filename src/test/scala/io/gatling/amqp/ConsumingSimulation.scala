3acb76447b225d714082c6ba282e3af78530fc7a
Ľubomír Varga <lubomir.varga@nike.sk>
2016-05-17 16:02:08 +0200

    Slightly modified ConsumingSimulation.scala example to show how to process (print) last consumed message.

--- src/test/scala/io/gatling/amqp/ConsumingSimulation.scala
+++ src/test/scala/io/gatling/amqp/ConsumingSimulation.scala
@@ -1,13 +1,10 @@
 package io.gatling.amqp
 
+import com.rabbitmq.client.QueueingConsumer.Delivery
 import io.gatling.amqp.Predef._
 import io.gatling.amqp.config._
-import io.gatling.amqp.data._
 import io.gatling.amqp.infra.AmqpConsumer
 import io.gatling.core.Predef._
-import io.gatling.core.assertion.FailedRequests
-
-import scala.concurrent.duration._
 
 class ConsumingSimulation extends Simulation {
   implicit val amqpProtocol: AmqpProtocol = amqp
@@ -15,8 +12,18 @@ class ConsumingSimulation extends Simulation {
     .port(5672)
     .auth("guest", "guest")
 
-  val scn = scenario("AMQP Consume").exec {
-    amqp("Consume").consume("q1")
+  val printConsumedMessages = true
+
+  val scn = scenario("AMQP Consume")
+    .exec {
+      amqp("Consume").consume("q1")
+    }.doIf(printConsumedMessages) {
+    // just for printing consumed messages
+    exec(session => {
+      val msg = session(AmqpConsumer.LAST_CONSUMED_MESSAGE_KEY).asOption[Delivery]
+      println("consumed msg = " + msg)
+      session
+    })
   }
 
   setUp(scn.inject(atOnceUsers(1))).protocols(amqpProtocol)
