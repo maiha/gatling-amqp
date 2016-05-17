95cefa2ed2c35cfba373f4170dacea76ee6caf39
Ľubomír Varga <lubomir.varga@nike.sk>
2016-05-17 16:12:41 +0200

    Fixed ConsumingSimulation.scala example code.

--- src/test/scala/io/gatling/amqp/ConsumingSimulation.scala
+++ src/test/scala/io/gatling/amqp/ConsumingSimulation.scala
@@ -16,14 +16,18 @@ class ConsumingSimulation extends Simulation {
 
   val scn = scenario("AMQP Consume")
     .exec {
-      amqp("Consume").consume("q1")
+      // consume single message (waiting till some message is in queue) and save it in session
+      amqp("Consume").consumeSingle("q1", saveResultToSession = printConsumedMessages)
     }.doIf(printConsumedMessages) {
     // just for printing consumed messages
     exec(session => {
       val msg = session(AmqpConsumer.LAST_CONSUMED_MESSAGE_KEY).asOption[Delivery]
-      println("consumed msg = " + msg)
+      println("consumed first msg = " + msg)
       session
-    })
+    }).exec {
+      //consume all messages which have left in queue
+      amqp("Consume").consumeSingle("q1")
+    }
   }
 
   setUp(scn.inject(atOnceUsers(1))).protocols(amqpProtocol)
